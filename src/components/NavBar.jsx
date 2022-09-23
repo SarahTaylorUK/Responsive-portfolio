4d58226b13545fb6632ddb3d8f6481ff9c24e7ac
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-09-23 11:00:37 +0100

    Added NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
+
+const NavBar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleClick = () => {
+        setNav(!nav)
+    }
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+        <div>
+            <p>logo</p>
+        </div>
+      {/* DESKTOP MENU */}
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      {/* HAMBURGER */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* MOBILE MENU */}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* CONTACTS */}
+      <div className="hidden"></div>
+    </div>
+  );
+};
+export default NavBar;
