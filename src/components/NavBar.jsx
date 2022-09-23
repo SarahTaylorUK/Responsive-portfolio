0eb1164c3858560ceb20f36c164927c282809f31
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-09-23 11:23:20 +0100

    Added responsive contacts bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,40 @@
 import React from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import { useState } from "react";
 
 const NavBar = () => {
-    const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
-    const handleClick = () => {
-        setNav(!nav)
-    }
+  const handleClick = () => {
+    setNav(!nav);
+  };
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-        <div>
-            <p>logo</p>
-        </div>
+      <div>
+        <p>logo</p>
+      </div>
       {/* DESKTOP MENU */}
-        <ul className="hidden md:flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
       {/* HAMBURGER */}
       <div onClick={handleClick} className="md:hidden z-10">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/* MOBILE MENU */}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Projects</li>
@@ -34,7 +42,42 @@ const NavBar = () => {
       </ul>
 
       {/* CONTACTS */}
-      <div className="hidden"></div>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              href="https://www.linkedin.com/in/sarah-taylor-19068413b/"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              href="https://github.com/SarahADev"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              href="mailto:sarah.alexandra12@gmail.com"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              href="mailto:sarah.alexandra12@gmail.com"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              CV <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
