a0d2bdf0efa47e4b62f3fa879a3695efc970a131
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-09-27 10:14:12 +0100

    Added View Projects button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
-// import { HiArrowNarrowRight } from "react-icons/hi";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -15,17 +16,20 @@ const Home = () => {
           Full-stack developer
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          Full-stack developer based in Scotland, with a background in Earth Science research.
+          Full-stack developer based in Scotland, with a background in Earth
+          Science research.
         </p>
-        {/* <div>
-          <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#3ab642] hover:border-[#3ab642] duration-200">
-            {" "}
-            View Projects
-            <span className="group-hover:rotate-90 duration-200">
-              <HiArrowNarrowRight className="ml-3" />
-            </span>
-          </button>
-        </div> */}
+        <div>
+          <Link to="projects" smooth={true} duration={500}>
+            <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#3ab642] hover:border-[#3ab642] duration-200">
+              {" "}
+              View Projects
+              <span className="group-hover:rotate-90 duration-200">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
